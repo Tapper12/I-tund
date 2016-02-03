9307cb51185cdb53925a9113e468a03dd1c3e3a9
Kevin Münter <Tapper12@users.noreply.github.com>
2016-02-03 10:35:03 +0200

    Added stuff

--- calc.js
+++ calc.js
@@ -1,3 +1,27 @@
 
-var cm;
-var inch;
+//Ootame kuni leht on laetud
+
+window.onload = function(){
+
+  //Saan kätte väljad
+  var cm = document.getElementById('cm');
+  var inch = document.getElementById('inch');
+
+  //lisan nuppudele kuularid
+
+  document.getElementById('to_inch').addEventListener('click', convertToInch);
+  document.getElementById('to_cm').addEventListener('click', convertToCm);
+
+};
+
+function convertToInch(){
+
+  inch.value = cm.value/2.54;
+
+}
+
+function convertToCm(){
+
+  cm.value = inch.value*2.54;
+
+}
